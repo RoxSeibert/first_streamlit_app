c7747b42a686416a3a5d192ce518c44c46d492da
Roxanne Seibert <91084940+RoxSeibert@users.noreply.github.com>
2023-02-10 10:52:59 -0500

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title( 'Mel's parents new healthy diner' )
+streamlit.title( 'My parents new healthy diner' )
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
